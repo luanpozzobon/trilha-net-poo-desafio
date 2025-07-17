2f673235ec16d20bbeff33c5068897f11e972935
Luan Pozzobon <luanpozzobon@gmail.com>
2025-07-17 10:19:01 -0300

    Implemented

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia("11", "Tijolão", "2023256245", 4);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Gmail");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("21", "X", "2025975763", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Gmail");
